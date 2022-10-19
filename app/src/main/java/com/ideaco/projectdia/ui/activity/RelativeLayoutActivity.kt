a940445caa720081ee9e9d8757ec73abc1c7db44
Stephen Daniel <stephendanielxxx@gmail.com>
2022-10-19 16:56:49 +0700

    day 2

--- app/src/main/java/com/ideaco/projectdia/ui/activity/RelativeLayoutActivity.kt
+++ app/src/main/java/com/ideaco/projectdia/ui/activity/RelativeLayoutActivity.kt
@@ -1,8 +1,9 @@
-package com.ideaco.projectdia
+package com.ideaco.projectdia.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.ideaco.projectdia.R
 
 class RelativeLayoutActivity : AppCompatActivity() {
 
@@ -12,8 +13,10 @@ class RelativeLayoutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relative_layout)
 
+        val text = intent.getStringExtra("key_hello")
+
         textView = findViewById(R.id.tvHello)
 
-        textView.text = "Hello World"
+        textView.text = text
     }
 }
